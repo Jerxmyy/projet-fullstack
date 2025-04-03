0f3cb0dacfcd201061fb087ca22323cd2ceb7519
jerxmy <jeremy.chambon@mail-esd.com>
2025-04-03 13:41:47 +0200

    refactorisation ButtonComponents

--- src/component/ButtonComponents.jsx
+++ src/component/ButtonComponents.jsx
@@ -1,85 +1,40 @@
 import React from "react";
-// Coucou Richnouuu c'est externalisé !!
+import "./Buttons.css";
 
-// CTA acceuil
+// Bouton de retour
 export const BackButton = ({ children, onClick }) => {
   return (
-    <button
-      className="back-button"
-      onClick={onClick}
-      style={{
-        backgroundColor: "black",
-        color: "white",
-        padding: "10px 20px",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-        position: "relative",
-        zIndex: 2,
-        margin: "20px",
-      }}
-    >
+    <button className="back-button" onClick={onClick}>
       {children}
     </button>
   );
 };
 
-// Choix Edition/Plateforme
+// Choix edition (si dispo)
 export const SelectionButton = ({ isSelected, onClick, children }) => {
   return (
     <button
+      className={`selection-button ${isSelected ? "selected" : ""}`}
       onClick={onClick}
-      style={{
-        padding: "8px 15px",
-        backgroundColor: isSelected ? "#706ad5" : "#333",
-        color: "white",
-        border: isSelected ? "2px solid white" : "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-      }}
     >
       {children}
     </button>
   );
 };
 
-// CTA Fav/Whislist
+// CTA Favoris/Wishlist
 export const IconButton = ({ onClick, children }) => {
   return (
-    <button
-      style={{
-        padding: "10px",
-        backgroundColor: "#706ad5",
-        color: "white",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-      }}
-      onClick={onClick}
-    >
+    <button className="icon-button" onClick={onClick}>
       {children}
     </button>
   );
 };
 
-// CTA ajouter au panier
+// CTA Panier
 export const PrimaryButton = ({ onClick, children }) => {
   return (
-    <button
-      style={{
-        flex: "1",
-        padding: "10px 20px",
-        backgroundColor: "#4CAF50",
-        color: "white",
-        border: "none",
-        borderRadius: "5px",
-        cursor: "pointer",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-      onClick={onClick}
-    >
+    <button className="primary-button" onClick={onClick}>
       {children}
     </button>
   );
