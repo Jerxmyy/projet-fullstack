7b60f1feb2f478b88bef59ebee9fb2c1d3892f93
jerxmy <jeremy.chambon@mail-esd.com>
2025-04-03 13:49:04 +0200

    refactorisation ListProduct

--- src/component/ListProduct.jsx
+++ src/component/ListProduct.jsx
@@ -1,60 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./ProductCard.css"; // Import du fichier CSS
 
 // Composant ProductCard -> carte cliquable
 const ProductCard = ({ id_products, title, price, img_url }) => {
   return (
-    <Link to={`/product/${id_products}`} style={{ textDecoration: "none" }}>
-      <div
-        style={{
-          width: "280px",
-          backgroundColor: "#1a1a1a",
-          borderRadius: "8px",
-          overflow: "hidden",
-          transition: "transform 0.2s",
-          flex: "0 0 auto",
-        }}
-        // hover
-        onMouseOver={(e) =>
-          (e.currentTarget.style.transform = "translateY(-5px)")
-        }
-        onMouseOut={(e) => (e.currentTarget.style.transform = "translateY(0)")}
-      >
+    <Link to={`/product/${id_products}`} className="product-card-link">
+      <div className="product-card">
         {/* Image du produit */}
-        <img
-          src={img_url}
-          alt={title}
-          style={{
-            width: "100%",
-            height: "400px",
-            objectFit: "cover",
-            display: "block",
-          }}
-        />
+        <img src={img_url} alt={title} className="product-image" />
         {/* Informations du produit */}
-        <div style={{ padding: "10px" }}>
-          <h6
-            style={{
-              color: "white",
-              fontSize: "14px",
-              marginBottom: "8px",
-              whiteSpace: "nowrap",
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-            }}
-          >
-            {title}
-          </h6>
+        <div className="product-info">
+          <h6 className="product-title">{title}</h6>
           {/* Affichage du prix */}
-          <div
-            style={{
-              backgroundColor: "#706ad5",
-              padding: "4px 8px",
-              borderRadius: "4px",
-              display: "inline-block",
-            }}
-          >
-            <span style={{ color: "white", fontWeight: "bold" }}>{price}€</span>
+          <div className="product-price">
+            <span>{price}€</span>
           </div>
         </div>
       </div>
