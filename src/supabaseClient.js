986a2316985c6ed24b68ebe3c574868bd1c3e0a4
jerxmy <jeremy.chambon@mail-esd.com>
2025-04-03 20:14:59 +0200

    FINAL (promis)

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supaUrl = process.env.SUPA_AURL;
+const supaUrl = process.env.SUPA_URL;
 const supaKey = process.env.SUPA_KEY;
 
 export const supabase = createClient(supaUrl, supaKey);
