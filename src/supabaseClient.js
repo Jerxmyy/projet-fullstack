f321b8fb3390ff01c8ffe5254d9f8a511a6f79b3
jerxmy <jeremy.chambon@mail-esd.com>
2025-04-04 15:05:14 +0200

    patch

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supaUrl = "SUPA_URL";
-const supaKey = "SUPA_KEY";
+const supaUrl = import.meta.env.VITE_SUPA_URL;
+const supaKey = import.meta.env.VITE_SUPA_KEY;
 
 export const supabase = createClient(supaUrl, supaKey);
